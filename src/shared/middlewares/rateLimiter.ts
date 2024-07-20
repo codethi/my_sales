c36838f945c9b4b3e7cdcf2d4e2cfae90b3e8143
codethi <docencia.thiago@gmail.com>
2024-07-20 11:53:39 -0300

    cache

---
+++ src/shared/middlewares/rateLimiter.ts
@@ -0,0 +1,32 @@
+import AppError from '@shared/errors/AppError';
+import { NextFunction, Request, Response } from 'express';
+import { RateLimiterRedis } from 'rate-limiter-flexible';
+import { createClient } from 'redis';
+
+const redisClient = createClient({
+  url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+  password: process.env.REDIS_PASS || undefined,
+});
+
+redisClient.connect().catch(console.error);
+
+const limiter = new RateLimiterRedis({
+  storeClient: redisClient,
+  keyPrefix: 'ratelimit',
+  points: 5,
+  duration: 5,
+});
+
+export default async function rateLimiter(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): Promise<void> {
+  try {
+    await limiter.consume(request.ip as string);
+
+    return next();
+  } catch (err) {
+    throw new AppError('Too many requests.', 429);
+  }
+}
